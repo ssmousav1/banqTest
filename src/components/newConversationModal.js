1027c41c11a4b6bbea93d34aba13a6f47fb6ebeb
Sobhan Mousavi <sobhanmousavi121122@yahoo.com>
2021-10-08 01:30:55 +0330

    new conversation modal style and request added

--- src/components/newConversationModal.js
+++ src/components/newConversationModal.js
@@ -5,25 +5,37 @@ import { API } from '../utils/APICalls';
 const NewConversation = (props) => {
 
   const [user, setUser] = useState(null)
+  const [conversationName, setConversationName] = useState('')
   const [conversation, setConversation] = useState(null)
 
   const [users, setUsers] = useState([])
 
   const getUsers = async () => {
     const res = await API('/user', 'get')
-    setUsers(res.data.data)
+    if (res.data) {
+      setUsers(res.data.data)
+    }
   }
 
   useEffect(() => {
     getUsers()
   }, [])
 
+  const postNewConversation = async () => {
+    let data = { user_ids: [] }
+    if (props.newConversationType === 'group') {
+      data.name = conversationName
+    }
+    const res = await API(`/user/${props.user.id}/conversation`, 'post', data)
+  }
 
   return (
     <div className="modal-container">
       <div className="modal-wrapper" onClick={() => props.setModal(false)}></div>
-      <span >
-        <h5>{props.newConversationType}</h5>
+      <span className="model-content">
+        <h5>{
+          props.newConversationType === 'single' ? 'Start a new conversation' : 'Start a new group chat'
+        }</h5>
         {
           props.newConversationType === 'single' ?
             <div>
@@ -36,9 +48,20 @@ const NewConversation = (props) => {
               </select>
             </div>
             :
-            <div></div>
+            <div>
+              <input type='text' value={conversationName} onChange={e => setConversationName(e.target.value)} />
+              <div>
+                <select multiple={true}>
+                  {
+                    users.map(user => (
+                      <option>{user.name}</option>
+                    ))
+                  }
+                </select>
+              </div>
+            </div>
         }
-        <button>
+        <button onClick={postNewConversation}>
           submit
         </button>
       </span>
