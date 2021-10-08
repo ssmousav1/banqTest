270a9a1c956ec5eb60663bdd177c973652debd8e
Sobhan Mousavi <sobhanmousavi121122@yahoo.com>
2021-10-09 00:28:34 +0330

    new conversaition functionality added with style

--- src/components/newConversationModal.js
+++ src/components/newConversationModal.js
@@ -4,11 +4,18 @@ import { API } from '../utils/APICalls';
 
 const NewConversation = (props) => {
 
-  const [user, setUser] = useState(null)
+  const [singleUser, setSingleUser] = useState(null)
+  const [groupUsers, setGroupUsers] = useState([])
   const [conversationName, setConversationName] = useState('')
   const [conversation, setConversation] = useState(null)
 
-  const [users, setUsers] = useState([])
+  const [users, setUsers] = useState([
+    { id: 1, name: 'ssn1' },
+    { id: 2, name: 'ssn2' },
+    { id: 3, name: 'ssn3' },
+    { id: 4, name: 'ssn4' },
+    { id: 5, name: 'ssn5' },
+  ])
 
   const getUsers = async () => {
     const res = await API('/user', 'get')
@@ -25,8 +32,29 @@ const NewConversation = (props) => {
     let data = { user_ids: [] }
     if (props.newConversationType === 'group') {
       data.name = conversationName
+      data.user_ids = groupUsers
+    } else {
+      data.user_ids = singleUser
     }
+    console.log(data);
     const res = await API(`/user/${props.user.id}/conversation`, 'post', data)
+  }
+
+  const selectSingleUser = e => {
+    setSingleUser(e.target.value)
+  }
+
+
+  const selectGroupUser = e => {
+    console.log(e.target.value);
+    let value = Array.from(
+      e.target.selectedOptions,
+      (option) => option.value
+    );
+    console.log(value);
+    setGroupUsers(
+      value,
+    );
   }
 
   return (
@@ -38,11 +66,11 @@ const NewConversation = (props) => {
         }</h5>
         {
           props.newConversationType === 'single' ?
-            <div>
-              <select>
+            <div style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
+              <select onChange={selectSingleUser} value={singleUser}>
                 {
                   users.map(user => (
-                    <option>{user.name}</option>
+                    <option value={user.id}>{user.name}</option>
                   ))
                 }
               </select>
@@ -50,11 +78,15 @@ const NewConversation = (props) => {
             :
             <div>
               <input type='text' value={conversationName} onChange={e => setConversationName(e.target.value)} />
-              <div>
-                <select multiple={true}>
+              <div style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
+                <select
+                  multiple={true}
+                  onChange={selectGroupUser}
+                  value={groupUsers}
+                >
                   {
                     users.map(user => (
-                      <option>{user.name}</option>
+                      <option value={user.id}>{user.name}</option>
                     ))
                   }
                 </select>
